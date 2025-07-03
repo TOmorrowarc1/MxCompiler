cc28898c6ccfdffac9dc4e00bba99f3311c2ec33
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-07-03 11:26:41 +0800

    feat: the package.

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,13 +9,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         String input = "1 + 2 - 3";  // 测试输入
         // 1. 创建词法分析器
-        ExprLexer lexer = new ExprLexer(CharStreams.fromString(input));
+        YxLexer lexer = new YxLexer(CharStreams.fromString(input));
         // 2. 创建Token流
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         // 3. 创建语法分析器
-        ExprParser parser = new ExprParser(tokens);
+        YxParser parser = new YxParser(tokens);
         // 4. 解析表达式
-        ParseTree tree = parser.expr();
+        ParseTree tree = parser.yx();
         // 5. 打印语法树
         System.out.println(tree.toStringTree(parser));
     }
