93eaa14ac008f59ac0a3c024cac1493df066421a
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-07-04 10:41:49 +0800

    feat: build the content of nodes' information.

---
+++ src/Utils/NodeInfo.java
@@ -0,0 +1,11 @@
+package Utils;
+
+public class NodeInfo {
+    Type type;
+    boolean isLeftValue;
+
+    public NodeInfo(Type type, boolean isLeftValue) {
+        this.type = type;
+        this.isLeftValue = isLeftValue;
+    }
+}
