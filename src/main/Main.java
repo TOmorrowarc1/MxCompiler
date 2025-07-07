56f23c3d9117c0bf79197a916d33bbb3679f48a7
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-07-07 18:25:52 +0800

    feat: compile.

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,28 +8,33 @@ import FrontEnd.*;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Scope globalScope = new Scope(null);
         InputStream input = System.in;
+        if (args.length > 0) {
+            String filePath = args[0];
+            input = new FileInputStream(filePath);
+        }
         try {
-
             YxLexer lexer = new YxLexer(CharStreams.fromStream(input));
 
             CommonTokenStream tokens = new CommonTokenStream(lexer);
 
             YxParser parser = new YxParser(tokens);
             ParseTree parserTree = parser.program();
-            ASTBuilder astBuilder = new ASTBuilder();
 
+            ASTBuilder astBuilder = new ASTBuilder();
             ProgramNode ASTRoot = (ProgramNode) astBuilder.visit(parserTree);
             new SymbolCollector(globalScope).visit(ASTRoot);
             new SemanticChecker(globalScope).visit(ASTRoot);
 
         } catch (SemanticError err) {
             System.out.println(err.getMessage());
+            System.exit(1);
         }
     }
 }
